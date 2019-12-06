d4b5a7b081bcce92aa27ec9fdea8bd31ff61f3e4
rlsurra <rlsurra@hotmail.com>
2019-12-06 15:40:43 -0300

    Interaciones varias

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import { planes } from './datosPlan.json';
+import Productos from '././Productos.js'
 
 class App extends Component {
 
   //Constructor define la estructura que permite almacenar en memoria (KISS)
   //Es el primer método que se ejecuta
 
-  constructor() {
-    super(); //Heredar funcionalidad de componente de react
-    this.state = {
-      planes
-    }
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      productos: [], suscripcion: ''
+    };
+
+    this.handleSearchButton = this.handleSearchButton.bind(this);
+}
+
+  handleSearchButton() {
+
+      fetch('/1169557094')
+      //fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ productos: data })
+      })
+      .catch(function(error) {
+        alert("La API productInventory no esta funcionando. Por favor, contactar al administrador")
+      }
+      )
   }
 
   //Pinta la pantalla
   render() {
-    const planes = this.state.planes.map((plan, i) => {
-      return (
-        <div className="card">
-          <h3> {plan.name} </h3>
-        </div>
-      )
-    })
-
     return (
       //React solo puede renderizar una etiqueta -> debemos contener todo en un div
       //<h1 className="display-1"> TITULO CON BOOTSTRAP </h1>
@@ -36,32 +42,76 @@ class App extends Component {
           <div className="header_content">
 
             <div className="logo">
-              <a href="">telefonica 3.0 </a>
-              <span>|| gestion simplificada de operaciones</span>
+              <label> telefonica 3.0 </label>
+              <span>  || gestion simplificada de operaciones</span>
             </div>
 
             <div className="menu">
               <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">about us</a></li>
+                <li><label>home</label></li>
+                <li><label>about us</label></li>
               </ul>
             </div>
 
           </div>
-        </div>  
-
-        <div id="slogan"> 
-            <div className="top_slogan"> 
-              Bienvenidos al CRM para la gestion simplificada de operaciones en T3.
-              Por favor, seleccione la operacion que desea realizar. 
-            </div>  
         </div>
 
+        <div className="slogan">
+          <div className="top_slogan">
+            Bienvenidos al CRM para la gestion simplificada de operaciones en T3.
+            Por favor, seleccione la operacion que desea realizar.
+            </div>
+        </div>
 
+        <div className="grilla">
+          <div class="row">
 
-      </div>
+              <div class="col-5">
+                <label for="filtro">Elija el filtro de busqueda <span>*</span> </label>
+                <div class="desplegable">
+                  <select name="filtro" class="form-control">
+                    <option disabled="disabled" selected="selected">Elija una opcion</option>
+                    <option value="1">Recurso primario</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="col-5">
+                <label for="valor">Valor <span>*</span></label>
+                <input type="text" class="form-control" required />
+              </div>
+
+              <div class="col-2 mb-3">
+                <br></br>
+                <button id="buscar" type="button" class="btn btn-primary float-right" onClick={this.handleSearchButton}> BUSCAR </button>
+
+              </div>
+
+            </div>
+
+          
+        </div>
+
+        <Productos productos={this.state.productos} />
+ 
+        </div>
     );
   }
 }
 
 export default App;
+
+/*
+
+            <div class="row-info">
+              <div class="col-md-6 mb-3">
+                <label for="first_name">PLAN</label>
+                <div id="namePlan" type="text" class="form-control"> Aca se mostrará el nombre de plan... </div>
+                </div>
+              <div class="col-md-6 mb-3">
+                <label for="last_name">DESCRIPCION</label>
+                <div id="descPlan" type="text" class="form-control"> Aca se mostrarán los detalles del plan... </div>
+              </div>
+            </div>
+
+*/
---
+++ src/Productos.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Producto.css';
+
+const Productos = ({ productos }) => {
+    return (
+    <div id="cartas-productos">
+        {productos.map((prod) => (
+        <div class="card">
+            <div class="card-body">
+                <h5 id="tipo-prod" class="card-title">{prod.productType}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{prod.name}</h6>
+                <p class="card-text">{prod.productOffering.description}</p>
+            </div>
+        </div>
+        ))}
+    </div>
+    )
+};
+
+export default Productos
+
+    /*
+
+                <h6 class="card-subtitle mb-2 text-muted">{prod.productOffering.id}</h6>
+                <p class="card-text">{prod.productOffering.name}</p>
+                <p class="card-text">{prod.productOffering.description}</p>
+
+    */
